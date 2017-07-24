570359210afb2f7b3ad3fee9128777a7202666ba
tetsurou21 <tetsurou21@gmail.com>
2017-07-24 22:13:57 +0900

    RHELを追加した。
    scriptsを更新

--- scripts/centos/cleanup.sh
+++ scripts/centos/cleanup.sh
@@ -1,13 +1,58 @@
-#!/bin/bash -eux
-# These were only needed for building VMware/Virtualbox extensions:
-# yum -y remove gcc cpp kernel-devel kernel-headers perl
-yum -y clean all
-rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
-rm -f /tmp/chef*rpm
+#!/bin/sh -eux
 
-# clean up redhat interface persistence
-rm -f /etc/udev/rules.d/70-persistent-net.rules
-if [ -r /etc/sysconfig/network-scripts/ifcfg-eth0 ]; then
-  sed -i 's/^HWADDR.*$//' /etc/sysconfig/network-scripts/ifcfg-eth0
-  sed -i 's/^UUID.*$//' /etc/sysconfig/network-scripts/ifcfg-eth0
+# should output one of 'redhat' 'centos' 'oraclelinux'
+distro="`rpm -qf --queryformat '%{NAME}' /etc/redhat-release | cut -f 1 -d '-'`" 
+
+# Remove development and kernel source packages
+yum -y remove gcc cpp kernel-devel kernel-headers perl;
+
+if [ "$distro" != 'redhat' ]; then
+  yum -y clean all;
 fi
+
+# Clean up network interface persistence
+rm -f /etc/udev/rules.d/70-persistent-net.rules;
+mkdir -p /etc/udev/rules.d/70-persistent-net.rules;
+rm -f /lib/udev/rules.d/75-persistent-net-generator.rules;
+rm -rf /dev/.udev/;
+
+for ndev in `ls -1 /etc/sysconfig/network-scripts/ifcfg-*`; do
+    if [ "`basename $ndev`" != "ifcfg-lo" ]; then
+        sed -i '/^HWADDR/d' "$ndev";
+        sed -i '/^UUID/d' "$ndev";
+    fi
+done
+
+# new-style network device naming for centos7
+if grep -q -i "release 7" /etc/redhat-release ; then
+  # radio off & remove all interface configration
+  nmcli radio all off
+  /bin/systemctl stop NetworkManager.service
+  for ifcfg in `ls /etc/sysconfig/network-scripts/ifcfg-* |grep -v ifcfg-lo` ; do
+    rm -f $ifcfg
+  done
+  rm -rf /var/lib/NetworkManager/*
+
+  echo "==> Setup /etc/rc.d/rc.local for CentOS7"
+  cat <<_EOF_ | cat >> /etc/rc.d/rc.local
+#BENTO-BEGIN
+LANG=C
+# delete all connection
+for con in \`nmcli -t -f uuid con\`; do
+  if [ "\$con" != "" ]; then
+    nmcli con del \$con
+  fi
+done
+# add gateway interface connection.
+gwdev=\`nmcli dev | grep ethernet | egrep -v 'unmanaged' | head -n 1 | awk '{print \$1}'\`
+if [ "\$gwdev" != "" ]; then
+  nmcli c add type eth ifname \$gwdev con-name \$gwdev
+fi
+sed -i "/^#BENTO-BEGIN/,/^#BENTO-END/d" /etc/rc.d/rc.local
+chmod -x /etc/rc.d/rc.local
+#BENTO-END
+_EOF_
+  chmod +x /etc/rc.d/rc.local
+fi
+
+rm -f VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?;
